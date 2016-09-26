1ff332196eb9d8f389af3716df92a3b4c45e0549
Edison Monsalve Cuartas <edison.monsalve@gmail.com>
2016-09-26 17:49:41 -0500

    Documentos base para Oráculo, versión web
    Los documentos con los que comienza todo el desarrollo.

---
+++ Generales/Docs/Estructura FAQ System Oráculo.mm
@@ -0,0 +1,657 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1473368470067" ID="ID_1293302723" MODIFIED="1473371285669">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Estructura FAQ System
+    </p>
+    <p>
+      Or&#225;culo
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1473368508546" ID="ID_655241521" MODIFIED="1473369424975" POSITION="right" TEXT="Visitante">
+<node CREATED="1473368508546" ID="ID_471675779" MODIFIED="1473369070357">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Inicio
+    </p>
+    <p>
+      index.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473368508546" ID="ID_876398526" MODIFIED="1473369077021" TEXT="Nosotros">
+<node CREATED="1473369082226" ID="ID_1229596028" MODIFIED="1473369116799">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mision
+    </p>
+    <p>
+      mision.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369089145" ID="ID_776148447" MODIFIED="1473369125038">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Vision
+    </p>
+    <p>
+      vision.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369097511" ID="ID_1980594173" MODIFIED="1473369130278">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Equipo
+    </p>
+    <p>
+      equipo.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1473368508546" ID="ID_707553005" MODIFIED="1473369231943">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Categor&#237;as
+    </p>
+    <p>
+      categorias.php
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1473369248677" ID="ID_342438918" MODIFIED="1473369266296">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      HTML
+    </p>
+    <p>
+      cat_html.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369248677" ID="ID_1216650100" MODIFIED="1473369281713">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CSS
+    </p>
+    <p>
+      cat_css.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369248677" ID="ID_1465884508" MODIFIED="1473369307453">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      JavaScript
+    </p>
+    <p>
+      cat_js.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369248677" ID="ID_540391497" MODIFIED="1473369357605">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sublime Text
+    </p>
+    <p>
+      cat_sublime.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369248677" ID="ID_924761149" MODIFIED="1473369373670">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Brackets
+    </p>
+    <p>
+      cat_brackets.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369248677" ID="ID_589841865" MODIFIED="1473369392279">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Otros
+    </p>
+    <p>
+      cat_otros.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1473368508546" ID="ID_1786436230" MODIFIED="1473369405012">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Contacto
+    </p>
+    <p>
+      contacto.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369467989" ID="ID_933516276" MODIFIED="1473369555950">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ingresar
+    </p>
+    <p>
+      Se ingresa o crea el usuario
+    </p>
+    <p>
+      ingresar.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1473368508546" ID="ID_402889528" MODIFIED="1473369513062" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Usuario
+    </p>
+    <p>
+      perfil editor
+    </p>
+    <p>
+      Ingresar - &gt; perfil.php
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1473369565512" ID="ID_1857370740" MODIFIED="1473369581165">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Crear pregunta
+    </p>
+    <p>
+      crear_pregunta.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473369588780" ID="ID_1930309028" MODIFIED="1473369602545">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ver usuarios
+    </p>
+    <p>
+      usuarios.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473369626214" ID="ID_1222085916" MODIFIED="1473370105949">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Salir
+    </p>
+    <p>
+      Redirige a index.php
+    </p>
+    <p>
+      salir.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1473368508546" ID="ID_67765968" MODIFIED="1473370281860" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Admin
+    </p>
+    <p>
+      en Ingresar.php debe haber un indicador de admin
+    </p>
+    <p>
+      Si es Admin, ingresar&#225; a
+    </p>
+    <p>
+      perfil_admin.php
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1473368508546" ID="ID_1177287610" MODIFIED="1473369513062">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Usuario
+    </p>
+    <p>
+      perfil editor
+    </p>
+    <p>
+      Ingresar - &gt; perfil.php
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1473369565512" ID="ID_1057786244" MODIFIED="1473369581165">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Crear pregunta
+    </p>
+    <p>
+      crear_pregunta.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369588780" ID="ID_1076315831" MODIFIED="1473369602545">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ver usuarios
+    </p>
+    <p>
+      usuarios.php
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473369626214" ID="ID_593628521" MODIFIED="1473370099578">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Salir
+    </p>
+    <p>
+      Redirige a index.php
+    </p>
+    <p>
+      salir.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1473369769500" ID="ID_1761890289" MODIFIED="1473369774327" TEXT="Categor&#xed;as">
+<node CREATED="1473369777179" ID="ID_1351030667" MODIFIED="1473369949534">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ver categorias
+    </p>
+    <p>
+      admin_ver_categorias.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473369798816" ID="ID_642202693" MODIFIED="1473369956670">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Modificar categor&#237;as
+    </p>
+    <p>
+      admin_modificar_categoria.php
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1473369840102" ID="ID_942604365" MODIFIED="1473369842460" TEXT="Titulo"/>
+<node CREATED="1473369847337" ID="ID_1721336735" MODIFIED="1473369853596" TEXT="P&#xe1;rrafo"/>
+<node CREATED="1473369855967" ID="ID_1669529909" MODIFIED="1473369858937" TEXT="Logo"/>
+</node>
+<node CREATED="1473369868243" ID="ID_15827623" MODIFIED="1473369962102">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Borrar Categorias
+    </p>
+    <p>
+      admin_borrar_categoria.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473369868243" ID="ID_1325099282" MODIFIED="1473369966908">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Crear Categorias
+    </p>
+    <p>
+      admin_crear_categoria.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1473370281861" ID="ID_473044854" MODIFIED="1473370285163" TEXT="Preguntas">
+<node CREATED="1473369777179" ID="ID_1791167391" MODIFIED="1473370306768">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ver preguntas
+    </p>
+    <p>
+      admin_ver_preguntas.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473369798816" ID="ID_972105837" MODIFIED="1473370324573">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Modificar preguntas
+    </p>
+    <p>
+      admin_modificar_preguntas.php
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1473369840102" ID="ID_1201119325" MODIFIED="1473369842460" TEXT="Titulo"/>
+<node CREATED="1473369847337" ID="ID_888977313" MODIFIED="1473369853596" TEXT="P&#xe1;rrafo"/>
+<node CREATED="1473369855967" ID="ID_1707275587" MODIFIED="1473369858937" TEXT="Logo"/>
+</node>
+<node CREATED="1473369868243" ID="ID_509511199" MODIFIED="1473370332637">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Borrar preguntas
+    </p>
+    <p>
+      admin_borrar_preguntas.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473369868243" ID="ID_783774591" MODIFIED="1473370342352">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Crear Categorias
+    </p>
+    <p>
+      admin_crear_preguntas.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1473369914702" ID="ID_962262479" MODIFIED="1473369929581">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Usuarios
+    </p>
+    <p>
+      admin_usuarios.php
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1473369970855" ID="ID_1156304431" MODIFIED="1473370044381">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ver usuarios
+    </p>
+    <p>
+      admin_ver_usuarios.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473369984230" ID="ID_29710740" MODIFIED="1473370035613">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Modificar usuarios
+    </p>
+    <p>
+      admin_mod_usuarios.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473369988474" ID="ID_495045495" MODIFIED="1473370022799">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Crear Admins
+    </p>
+    <p>
+      admin_crea_admin.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1473370129887" ID="ID_1637993322" MODIFIED="1473370132081" TEXT="Sitio">
+<node CREATED="1473370134028" ID="ID_488013493" MODIFIED="1473370147213">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mision
+    </p>
+    <p>
+      admin_mision.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473370134028" ID="ID_311555554" MODIFIED="1473370160508">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Visi&#243;n
+    </p>
+    <p>
+      admin_vision.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473370134028" ID="ID_213438475" MODIFIED="1473370196856">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Equipo
+    </p>
+    <p>
+      admin_equipo.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1473370134028" ID="ID_1849516217" MODIFIED="1473370213572">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Contacto
+    </p>
+    <p>
+      admin_contacto.php
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1473371288591" ID="ID_788191589" MODIFIED="1473371296340" POSITION="right" TEXT="Carpetas iniciales">
+<node CREATED="1473371307696" ID="ID_780246453" MODIFIED="1473371310105" TEXT="css"/>
+<node CREATED="1473371312013" ID="ID_569849595" MODIFIED="1473371317973" TEXT="js"/>
+<node CREATED="1473371322682" ID="ID_1425672267" MODIFIED="1473371325071" TEXT="fonts"/>
+<node CREATED="1473371327336" ID="ID_666433596" MODIFIED="1473371329962" TEXT="core"/>
+<node CREATED="1473371331155" ID="ID_1342633241" MODIFIED="1473371332788" TEXT="img"/>
+<node CREATED="1473371333972" ID="ID_30744529" MODIFIED="1473371335496" TEXT="imagenes"/>
+<node CREATED="1473371336312" ID="ID_1496039690" MODIFIED="1473371337296" TEXT="otros"/>
+</node>
+</node>
+</map>
